9f681b5f9b9a2eced002fa9f40c3bc7a0a92b1d1
Wojciech Puchalski <wojciech.puchalski@fnx-group.com>
2024-09-25 07:30:56 +0200

    Jenkins Tests

--- src/test/java/RestAssuredTest.java
+++ src/test/java/RestAssuredTest.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 import static io.restassured.RestAssured.*;
 
-//several advanced API tests
+//several advanced RestAssured API tests
 @Test
 public class RestAssuredTest {
 
